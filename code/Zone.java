d98235e39861ec408ed8d462c5eb2b4af45e5041
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2024-12-24 19:20:36 +0100

    Update Zone.java

--- code/Zone.java
+++ code/Zone.java
@@ -1,11 +1,62 @@
 public class Zone {
 
 	private int idZone;
-
+	private Jeton jeton;
+	private Cout cout;
 	private boolean etat;
-
-	public boolean isActif() {
-		return false;
+	
+	private static int compteur = 0;
+	
+	public Zone(){
+		this.idZone=++compteur;
+		this.jeton = new Jeton();
+		this.cout = new Cout();
+		this.etat=true;
+	}
+	
+	public Zone(Jeton j, Cout c, boolean b){
+		this.idZone=++compteur;
+		this.jeton = j;
+		this.cout = c;
+		this.etat=b;
+	}
+	
+	public void isActif() {
+		if (this.etat==false) {
+			this.etat=true;
+		}else {
+			this.etat=false;
+		}
+	}
+	
+	public boolean getEtat() {
+		return this.etat;
+	}
+	public void setEtat(boolean b) {
+		this.etat=b;
+	}
+	// METHODES JETON
+	
+	public String getCouleur() {
+		return jeton.getCouleur();
+	}
+	public void setCouleur(String s) {
+		jeton.setCouleur(s);
+	}
+	public int getIdJeton() {
+		return jeton.getId();
+	}
+	public void inverserCouleur() {
+		jeton.inverserCouleur();
+	}
+	
+	// METHODES COUT
+	public int getMontant() {
+		return cout.getMontant();
+	}
+	
+	public void setMontant(int x) {
+		cout.setMontant(x);
 	}
 
 }
