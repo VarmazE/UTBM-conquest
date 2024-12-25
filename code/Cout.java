7bf0d1dddf8b5e4bbfb2a0e1804fbb33070d0167
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2024-12-25 13:59:10 +0100

    Update Cout.java

--- code/Cout.java
+++ code/Cout.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Cout {
 
 	private int montant;
+	private ArrayList<Ressources> listRessources;
+	
+	public Cout() {
+		this.montant =0;
+		this.listRessources= new List<Ressources>;
+	}
+	
+	public Cout(int x, ArrayList<Ressources> lr) {
+		this.montant =x;
+		this.listRessources= lr;
+	}
+	
+	
+	public int getMontant() {
+		return this.montant;
+	}
+	public void setMontant(int x) {
+		this.montant=x;
+	}
+
 
 }
